47141651aa17ef63450c610d263210d01ab5a734
ArturStachera <r2r2stachera@gmail.com>
2026-01-21 18:47:23 +0100

    Prevent duplicate pcspkr blacklist entry

--- configuration/beep.sh
+++ configuration/beep.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo sh -c 'echo "blacklist pcspkr" >> /etc/modprobe.d/blacklist.conf'
+if ! sudo grep -q '^blacklist pcspkr$' /etc/modprobe.d/blacklist.conf 2>/dev/null; then
+    sudo sh -c 'echo "blacklist pcspkr" >> /etc/modprobe.d/blacklist.conf'
+fi
